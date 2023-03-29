184c2c35029e96878f275800aa4f2a77aaff9d27
Ana Mello <G00425653@atu.ie>
2023-03-29 23:20:15 +0100

    project

--- analysis.py
+++ analysis.py
@@ -4,3 +4,15 @@
 ## 3. Outputs a scatter plot of each pair of variables.
 ## 4. Performs any other analysis you think is appropriate
 
+
+
+
+## my reaserch : 
+## http://www.lac.inpe.br/~rafael.santos/Docs/CAP394/WholeStory-Iris.html
+## https://levelup.gitconnected.com/unveiling-the-mysteries-of-the-iris-dataset-a-comprehensive-analysis-and-machine-learning-f5c4f9dbcd6d
+## https://medium.com/analytics-vidhya/exploratory-data-analysis-iris-dataset-4df6f045cda#:~:text=Data%20Insights%3A,-The%20pdf%20curve&text=If%20petal%20length%20%3C%202.1%2C%20then,then%20species%20is%20Iris%20Virginica
+## https://www.kaggle.com/code/lalitharajesh/iris-dataset-exploratory-data-analysis
+## https://www.geeksforgeeks.org/exploratory-data-analysis-on-iris-dataset/
+## https://www.kaggle.com/code/zachgold/python-iris-data-visualizations
+## https://www.analyticsvidhya.com/blog/2021/06/guide-to-data-visualization-with-python-part-1/
+
