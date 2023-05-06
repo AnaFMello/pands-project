8472d7c290d11a2cc480cf089eabf2f19949436b
Ana Mello <G00425653@atu.ie>
2023-05-06 16:50:26 +0100

    Pands project 2023

--- README.md
+++ README.md
@@ -2,53 +2,39 @@
 Author: Ana Mello
 
 Summary Of The Fisher's Iris Dataset
-
 The Iris flower dataset is a well-known multivariate dataset that was designed by British statistician and geneticist Ronald Aylmer Fisher as an example of discriminant analysis. It includes 150 cases (rows) and 5 variables (columns), which are Sepal.Length, Sepal.Width, Petal.Length, Petal.Width, and Species. The data comprises of three different species of the Iris flower: Iris setosa, Iris virginica, and Iris versicolor.
-
 The primary objective of the discriminant analysis is to create a simple function that can classify a flower correctly based on the four measurements. This forms the foundation of developing "predictors" to enhance accuracy in dataset records.
-
 In 1935, an American botanist named Edgar Anderson gathered data on the three distinct Iris species from the Gaspé Peninsula in Québec province, eastern Canada. The specimens were collected on the same day, by the same person, and measured using the same instruments.
-
 Ronald Fisher utilized Anderson's data in 1936 to determine whether linear regression could be used to maximize the ratio of the difference between the specific means to the standard deviations within species. As a result, the Iris flower dataset has gained immense recognition as the "Hello World" in Data Analysis.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Code Explanation:
+(Step by step is explained in the code.)
+The code in this file, analysis.py, performs several analyses on the Iris dataset (iris.data), which contains measurements of the length and width of petals and sepals of three different species of Iris flowers.
+The first part of the code reads in the Iris dataset using pandas, and then computes a summary of each variable using the describe() function. This summary is written to a text file called summary.txt.
+The second part of the code creates a histogram for each variable and saves each histogram as a png file. These histograms are plotted using matplotlib.
+Next, the code performs some data cleaning by splitting the "iris-species" column into two separate columns, one for the species name and one for the variety name. It then drops the original "iris-species" column and concatenates the new columns back onto the dataset.
+The code then creates a scatter plot of petal length vs. petal width, colored by species using the seaborn library. It also creates a violin plot for each variable, showing the distribution of each variable by species.
+Finally, the code creates a pairplot using seaborn to show the relationship between all pairs of variables, colored by species. This pairplot is saved as a png file.
+Overall, this code provides a thorough analysis of the Iris dataset, including descriptive statistics, visualizations of the data, and data cleaning.
 
 Reaserch:
-
+https://pandas.pydata.org/docs/
 https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.describe.html
+https://www.kaggle.com/learn/overview
 http://www.lac.inpe.br/~rafael.santos/Docs/CAP394/WholeStory-Iris.html
-## https://levelup.gitconnected.com/unveiling-the-mysteries-of-the-iris-dataset-a-comprehensive-analysis-and-machine-learning-f5c4f9dbcd6d
-## https://medium.com/analytics-vidhya/exploratory-data-analysis-iris-dataset-4df6f045cda#:~:text=Data%20Insights%3A,-The%20pdf%20curve&text=If%20petal%20length%20%3C%202.1%2C%20then,then%20species%20is%20Iris%20Virginica
-## https://www.kaggle.com/code/lalitharajesh/iris-dataset-exploratory-data-analysis
-## https://www.geeksforgeeks.org/exploratory-data-analysis-on-iris-dataset/
-## https://www.kaggle.com/code/zachgold/python-iris-data-visualizations
-## https://www.analyticsvidhya.com/blog/2021/06/guide-to-data-visualization-with-python-part-1/
-
+https://levelup.gitconnected.com/unveiling-the-mysteries-of-the-iris-dataset-a-comprehensive-analysis-and-machine-learning-f5c4f9dbcd6d
+https://realpython.com/
+https://numpy.org/doc/
+https://docs.python.org/
+https://www.datacamp.com/community/tutorials/seaborn-python-tutorial
+https://medium.com/analytics-vidhya/exploratory-data-analysis-iris-dataset-4df6f045cda#:~:text=Data%20Insights%3A,-The%20pdf%20curve&text=If%20petal%20length%20%3C%202.1%2C%20then,then%20species%20is%20Iris%20Virginica
+https://jupyter-notebook.readthedocs.io/en/stable/
+https://www.kaggle.com/code/lalitharajesh/iris-dataset-exploratory-data-analysis
+https://towardsdatascience.com/
+https://www.geeksforgeeks.org/exploratory-data-analysis-on-iris-dataset/
+https://www.kaggle.com/code/zachgold/python-iris-data-visualizations
+https://stackoverflow.com/
+https://www.analyticsvidhya.com/blog/2021/06/guide-to-data-visualization-with-python-part-1/
+https://archive.ics.uci.edu/ml/datasets/iris
+https://seaborn.pydata.org/
+https://matplotlib.org/stable/contents.html
--- analysis.py
+++ analysis.py
@@ -1,27 +1,91 @@
-## This program is able to:
-## 1. Outputs a summary of each variable to a single text file,
-## 2. Saves a histogram of each variable to png files, and
-## 3. Outputs a scatter plot of each pair of variables.
-## 4. Performs any other analysis you think is appropriate
+## Analysis.py
+## Author: Ana Figueiro
 
-
+## Importing librariers to be able to use  the functions and methods they provide to perform data analysis and visualization tasks.
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
-# Load the Iris dataset from the CSV file into a DataFrame
-df = pd.read_csv("iris.data")
+## Loading the dataset: The first step is to load the iris dataset using pandas' read_csv() function. 
+## The dataset is stored in a CSV file, and the read_csv() function is used to read the file and create a pandas DataFrame object called iris. 
+## The sep parameter specifies the delimiter used in the CSV file, and names parameter is used to assign meaningful names to each column of the dataset.
+iris = pd.read_csv('iris.data', sep=',', names=['sepal.length', 'sepal.width', 'petal.length', 'petal.width', 'iris-species'])
 
-# Output a summary of each variable to a text file
-with open("variable_summaries.txt", "w") as file:
-    file.write(str(df.describe()))
+## Generating a summary of the data: The describe() function is used to generate a summary of each variable in the iris dataset. 
+# The T function is used to transpose the summary table so that it is easier to read. The summary is then saved to a text file using Python's open() function in write mode ('w').
+summary = iris.describe().T
+with open('summary.txt', 'w') as f:
+    f.write(str(summary))
 
-# Save a histogram of each variable to PNG files
-for column in df.columns[:-1]:
+## Creating histograms: A for loop is used to create a histogram of each variable in the iris dataset. 
+# The hist() function is used to generate the histograms, and the resulting plots are saved to PNG files using the savefig() function.
+for col in iris.columns[:-1]:
     plt.figure()
-    sns.histplot(data=df, x=column)
-    plt.savefig(f"{column}_histogram.png")
+    iris.hist(column=col, bins=10, grid=False, edgecolor='black')
+    plt.title(col)
+    plt.xlabel(col)
+    plt.ylabel('Frequency')
+    plt.savefig(f'{col}_histogram.png', dpi=300)
 
-# Output a scatter plot of each pair of variables
-##sns.pairplot(df, hue="species")
-##plt.savefig("scatter_plots.png")
+
+## Preparing the data for analysis: The iris dataset is further prepared for analysis by creating a new 
+# DataFrame called iris2 that contains the species column split into two separate columns, and then renamed to species. The iris and iris2 
+# DataFrames are then concatenated using the concat() function, and the resulting DataFrame is assigned back to iris. 
+# The iris-species column is dropped from the iris DataFrame.
+iris2 = iris['iris-species'].str.split('-', n=1, expand=True)
+iris2 = iris2.drop(columns=0)
+iris2 = iris2.rename(columns={1: "species"})
+iris = pd.concat([iris, iris2], axis=1)
+iris = iris.drop(columns='iris-species')
+iris.head()
+iris.info()
+
+## Generating a scatter plot: A scatter plot of petal width vs. length is generated using the scatterplot() 
+# function from the seaborn library. The resulting plot is saved to a PNG file.
+plt.figure(figsize=(8,6))
+sns.scatterplot(data=iris, x='petal.length', y='petal.width', hue='species')
+plt.title('Petal Width vs Length')
+plt.xlabel('Width')
+plt.ylabel('Length')
+plt.show()
+
+## Generating summary statistics for the setosa species: The minimum and maximum values for each 
+# variable in the iris dataset for the setosa species are computed using the min() and max() functions, 
+# and the resulting DataFrames are concatenated using the concat() function. The resulting DataFrame is printed to the console.
+setosa_min = pd.DataFrame(iris[iris.species == 'setosa'].min(), columns=['minimum'])[0:4]
+setosa_max = pd.DataFrame(iris[iris.species == 'setosa'].max(), columns=['maximum'])[0:4]
+pd.concat([setosa_min, setosa_max], axis=1)
+
+
+## Generating a histogram for each variable in the iris dataset: Using `iris.hist()`. The `figsize` parameter sets the size of the figure, 
+# and `edgecolor` and `linewidth` parameters set the color and width of the borders for each histogram bar, it will result in histograms are displayed using `plt.show()`.
+iris.hist(figsize=(12,6), edgecolor='black', linewidth=0.3)
+plt.show()
+
+
+## Creating a grid of scatter plots: Using seaborn's pairplot() function, showing the relationship between each pair of variables in the iris dataframe. 
+# It colors each point based on the 'species' column, and displays the plot.
+sns.pairplot(iris, hue= 'species');
+
+## This code creates four violin plots using the Seaborn library `sns.violinplot()`. 
+# Each plot displays a different variable (`petal.length`, `petal.width`, `sepal.length`, and `sepal.width`) on the y-axis, and the species of iris on the x-axis. 
+# The resulting violin plots are displayed using `plt.show()`.
+plt.figure(figsize=(10,8))
+plt.subplot(2,2,1)
+sns.violinplot(x='species', y='petal.length', data=iris)
+plt.subplot(2,2,2)
+sns.violinplot(x='species', y='petal.width', data=iris)
+plt.subplot(2,2,3)
+sns.violinplot(x='species', y='sepal.length', data=iris)
+plt.subplot(2,2,4)
+sns.violinplot(x='species', y='sepal.width', data=iris)
+plt.show()
+
+## This code creates a scatterplot matrix using the Seaborn library `sns.pairplot()`. 
+# The `hue` parameter is set to 'species', which colors the scatterplot points by the species of iris being represented. 
+# The resulting scatterplot matrix is saved as a PNG file named 'scatter_plot.png' using `plt.savefig()`, and is also displayed using `plt.show()`.
+sns.pairplot(iris, hue='species')
+plt.savefig('scatter_plot.png', dpi=300)
+plt.show()
+
